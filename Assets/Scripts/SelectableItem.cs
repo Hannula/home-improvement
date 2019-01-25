d3882edf908de8c8bf62aecb8dbd80e68dbd001d
Hannula <hannula.rama.p@student.uta.fi>
2019-01-25 23:52:42 +0200

    Add BattleScene and floor prefab.

---
+++ Assets/Scripts/SelectableItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectableItem : MonoBehaviour
+{
+    
+    public bool Selected;
+    public bool HoveringOver;
+
+    // Everytime item is clicked
+    public void OnClick()
+    {
+
+    }
+
+    // Only when selected the first time
+    public void OnSelect()
+    {
+
+    }
+
+    public void OnUnselect()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/UpgradeSlot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeSlot : SelectableItem
+{
+    public bool IsEmpty { get { return true; } }
+
+    public SpriteRenderer SpriteRenderer;
+    public Color SelectedColor;
+    public Color EmptyColor;
+    public Color HoverColor;
+    public Color FilledColor;
+
+    public void Update()
+    {
+        if (IsEmpty)
+        {
+            SpriteRenderer.color = EmptyColor;
+        }
+        if (!IsEmpty)
+        {
+            SpriteRenderer.color = FilledColor;
+        }
+        if (HoveringOver)
+        {
+            SpriteRenderer.color = HoverColor;
+        }
+        if (Selected)
+        {
+            SpriteRenderer.color = SelectedColor;
+        }
+    }
+
+}
