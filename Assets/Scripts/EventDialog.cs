740261b4810bf18b5003f552c78f9c3e935149c3
Lauri Kosonen <lauri.kosonen@cs.tamk.fi>
2019-01-26 02:00:50 +0200

    Created EventDialog

--- Assets/Scripts/EventDialog.cs
+++ Assets/Scripts/EventDialog.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class EventDialog : MonoBehaviour
+public class EventDialog : UIScreen
 {
+    public EventButton[] buttons;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
+    {
+        buttons = GetComponentsInChildren<EventButton>();
+    }
+
+    // Update is called once per frame
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DisplayButtons(string[] buttonNames)
     {
-        
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (i < buttonNames.Length)
+            {
+                buttons[i].title = buttonNames[i];
+                buttons[i].Activate(true);
+            }
+            else
+            {
+                buttons[i].Activate(false);
+            }
+        }
     }
 }
