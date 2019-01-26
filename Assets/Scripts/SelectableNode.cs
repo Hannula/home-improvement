f70d5d4bae231f44f08584c5ca5d100b4834152e
Lauri Kosonen <lauri.kosonen@cs.tamk.fi>
2019-01-26 14:38:06 +0200

    Created EventManager

--- Assets/Scripts/SelectableNode.cs
+++ Assets/Scripts/SelectableNode.cs
@@ -7,16 +7,23 @@ public class SelectableNode : SelectableItem
 {
     public Node node;
 
+    private EventManager eventManager;
+
+    private void Start()
+    {
+        eventManager = FindObjectOfType<EventManager>();
+    }
+
     // Everytime item is clicked
     public override void OnClick()
     {
-
+        
     }
 
     // Only when selected the first time
     public override void OnSelect()
     {
-
+        eventManager.StartEvent(node);
     }
 
     public override void OnUnselect()
